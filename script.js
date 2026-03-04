ea2542fe98ac417164f7cc805853388cc78e2c42
Adith Raj <137923304+Solo-Being@users.noreply.github.com>
2026-03-04 16:12:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let  mennu=document.querySelector('.menuu')
+    function  menu_drop(){
+        if(mennu.style.display==='block'){
+            mennu.style.display='none'
+        }
+        else{
+            mennu.style.display='block'
+        }
+    }
